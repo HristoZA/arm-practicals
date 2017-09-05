f35b6c1051ac84d6bb8b1ef3786f1795ab50c13d
Hristo Mitev <hristo2404@gmail.com>
2017-09-05 14:04:16 +0200

    Update 3556336_prac3q2a.s

--- prac3/q2/3556336_prac3q2a.s
+++ prac3/q2/3556336_prac3q2a.s
@@ -1,54 +1,56 @@
+/* -- printf01.s */
 .data
-
+ 
+/* First message */
 .balign 4
-prompt_num1: .asciz "Please enter number 1: "
-
+message1: .asciz "Hey, type a number: "
+ 
+/* Second message */
 .balign 4
-prompt_num2: .asciz "\nPlease enter number 2: "
-
+message2: .asciz "I read the number %d\n"
+ 
+/* Format pattern for scanf */
 .balign 4
-output: .asciz "Number 1 is %d\n"
-
+scan_pattern : .asciz "%d"
+ 
+/* Where scanf will store the number read */
 .balign 4
-scan_pattern: .asciz "%d"
-
-.balign 4
-num_reader: .word 0
-
+number_read: .word 0
+ 
 .balign 4
 return: .word 0
-
-.text 
+ 
+.text
+ 
 .global main
 main:
-	ldr r1, adr_return
-	str lr, [r1]
-	
-	ldr r0, adr_prompt_num1
-	bl printf
-	
-	ldr r0, adr_scan_pattern
-	ldr r1, adr_num_reader
-	bl scanf
-	
-	ldr r0, =adr_output
-	ldr r1, adr_num_reader
-	ldr r1, [r1]	
-	bl printf
-
-	ldr r0, adr_num_reader
-	ldr r0, [r0]
-		
-	ldr lr, adr_return
-	ldr lr, [lr]
-	bx lr
-	
-adr_prompt_num1: .word prompt_num1
-adr_prompt_num2: .word prompt_num2
-adr_output: .word output
-adr_scan_pattern: .word scan_pattern
-adr_num_reader: .word num_reader
-adr_return: .word return
-
+    ldr r1, address_of_return        /* r1 ← &address_of_return */
+    str lr, [r1]                     /* *r1 ← lr */
+ 
+    ldr r0, address_of_message1      /* r0 ← &message1 */
+    bl printf                        /* call to printf */
+ 
+    ldr r0, address_of_scan_pattern  /* r0 ← &scan_pattern */
+    ldr r1, address_of_number_read   /* r1 ← &number_read */
+    bl scanf                         /* call to scanf */
+ 
+    ldr r0, address_of_message2      /* r0 ← &message2 */
+    ldr r1, address_of_number_read   /* r1 ← &number_read */
+    ldr r1, [r1]                     /* r1 ← *r1 */
+    bl printf                        /* call to printf */
+ 
+    ldr r0, address_of_number_read   /* r0 ← &number_read */
+    ldr r0, [r0]                     /* r0 ← *r0 */
+ 
+    ldr lr, address_of_return        /* lr ← &address_of_return */
+    ldr lr, [lr]                     /* lr ← *lr */
+    bx lr                            /* return from main using lr */
+address_of_message1 : .word message1
+address_of_message2 : .word message2
+address_of_scan_pattern : .word scan_pattern
+address_of_number_read : .word number_read
+address_of_return : .word return
+ 
+/* External */
 .global printf
 .global scanf
