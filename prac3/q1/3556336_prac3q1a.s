8b00c6ef30ec901f643ef8fcdcb8b2c9a29ea2aa
HristoZA <3556336@myuwc.ac.za>
2017-09-05 10:22:09 +0000

    q1a added

---
+++ prac3/q1/3556336_prac3q1a.s
@@ -0,0 +1,58 @@
+.data
+.balign 4
+prompt_number1: .asciz "\nEnter Number 1\n"
+prompt_number2: .asciz "\nEnter Number 2\n"
+
+.balign 4
+number_format: .asciz "%d"
+
+.balign 4
+number_response: .int 0
+
+.text
+.global main
+.extern printf
+.extern scanf
+main:
+	push {ip, lr}
+
+	ldr R0, =prompt_number1
+	bl printf
+
+	ldr R0, =number_format
+	ldr R1, =number_response
+	bl scanf
+
+	ldr R0, =number_response
+	ldr R0, [R0]
+
+	mov r2, r1
+
+	ldr R0, =prompt_number2
+	bl printf
+
+	ldr R0, =number_format
+	ldr R1, =number_response
+	bl scanf
+
+	ldr R1, =number_response
+	ldr R1, [R1]
+
+	mov r3, r1
+	CMP R2, r3
+
+	BEQ if_equal
+
+	if_equal :
+		mov r0, r2
+		b end
+	not_equal:
+		mov r0,r3
+		b end
+	
+	end:
+
+		pop {ip, pc}
+		
+	
+	
