7898ece59483dc6af5dbf4b738614150f9112014
Hristo Mitev <hristo2404@gmail.com>
2017-09-12 19:25:01 +0200

    Update 3556336_prac4q1.s

--- prac4/q1/3556336_prac4q1.s
+++ prac4/q1/3556336_prac4q1.s
@@ -1,26 +1,26 @@
 .data
 
 .align 4
-arr: .skip 10
+arr: .skip 40		@ Create space for an array of size 10
 
 .text
 .global main
 main:
 
-	ldr r1, adr_arr
-	mov r2, #0 
+	ldr r1, adr_arr				@ load array into r1
+	mov r2, #0 				@ set r2 to 0 (as a counter)
 	
 	loop:
-		cmp r2, #10
-		beq end
-		add r3, r2, #1
-		str r3, [r1, +r2, LSL #2]
-		add r2, r2, #1
+		cmp r2, #10			@ Compare r2 to size of array
+		beq end				@ if r2 = size of array, branch to end
+		add r3, r2, #1			@ The value to be written at the position r2 in array
+		str r3, [r1, +r2, LSL #2]	@ Store r3 in the base address (r1) + r2 *4
+		add r2, r2, #1			@ increase count of r2 by 1
 		
-		b loop
+		b loop				@ Begin loop again
 	end:
-		mov r0, #0
-		bx lr
+		mov r0, #0			@ Set exit status to 0
+		bx lr				@ Exit the program
 	
 	
 adr_arr: .word arr
