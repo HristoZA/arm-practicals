eb58afbf63af2684901861a603b5957126a7ee46
Hristo Mitev <3556336@myuwc.ac.za>
2017-11-01 17:23:46 +0000

    some work

--- prac8/q1/3556336_prac8q1.s
+++ prac8/q1/3556336_prac8q1.s
@@ -3,9 +3,7 @@
 errmsg: .asciz "create failed"
 errmsgend:
 newfile: .asciz "/home/pi/newfile"
-prompt: .asciz "Input a string: \n"
-promptend:
-buffer: .byte 100
+arr: .word 5, 1, 3, 6, 7, 8, 4, 9, 210
 
 .text
 .global main    
@@ -25,20 +23,26 @@ main:
 
  mov r4, r0 @ save file_descriptor
 
- /* READ */
- mov r0, #0 @ stdin (keyboard)
- ldr r1, =buffer @ address of buffer
- mov r2, #26 @ maximum length of input
- mov r7, #3 @ read
- svc 0
- mov r5, r0 @ save number of characters
+ ldr r8, =arr
+ mov r9, #9
+ mov r10, #0
+
+loop:
 
  /* WRITE TO FILE */
+ cmp r10, r9
+ beq end_loop
+
  mov r0, r4 @ file_descriptor
- ldr r1, =buffer @ address of buffer
- mov r2, r5 @ length from read
+ ldr r1, [r8, +r10, LSL #2]
+ mov r2, #1 @ length from read
  mov r7, #4 @ write
  svc 0
+
+ add r10, r10, #1
+ b loop
+
+end_loop:
 
  /* CLOSE FILE */
  mov r7, #6 @ close
